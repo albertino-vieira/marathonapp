f1c8e805a87cc363f381a1aff56e49437e1334b3
AlbertinoVieira <8140346@estg.ipp.pt>
2026-03-17 10:33:23 +0000

    Initial Commit

---
+++ marathonapp/marathonappApp.swift
@@ -0,0 +1,17 @@
+//
+//  marathonappApp.swift
+//  marathonapp
+//
+//  Created by Albertino Vieira on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct marathonappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
